5ad84ecc0e0a5e3bcea75e6b01c0848f461ba013
dei0811 <deinergonzalezparedes12@gmail.com>
2026-04-17 12:36:15 -0500

    debuggin login api conection

---
+++ SGS/src/app/services/auth.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://127.0.0.1:8000/token'; // endpoint OAuth2 estándar
+
+  constructor(private http: HttpClient) {}
+
+  login(data: { email: string; password: string }) {
+    const body = new HttpParams()
+      .set('username', data.email)   // OAuth2 usa "username", no "email"
+      .set('password', data.password)
+      .set('grant_type', 'password'); // requerido en OAuth2
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+
+    return this.http.post<any>(this.apiUrl, body.toString(), { headers });
+  }
+}
