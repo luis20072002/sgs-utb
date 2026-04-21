776ed535592f08836fcd48bc820444439a9a7a8d
dei0811 <deinergonzalezparedes12@gmail.com>
2026-04-21 09:35:16 -0500

    added dashboard to work with

--- SGS/src/app/pages/login/login.ts
+++ SGS/src/app/pages/login/login.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RedirectCommand, Route } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.html',
@@ -12,11 +14,13 @@ import { CommonModule } from '@angular/common';
 })
 export class Login {
 
+  
   form: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    private auth: AuthService
+    private auth: AuthService,
+    private router:Router
   ) {
     this.form = this.fb.group({
       email: ['', [Validators.required]],
@@ -25,17 +29,15 @@ export class Login {
   }
 
 login() {
-   alert('login ejecutado');
-   console.log('form válido:', this.form.valid);
-  console.log('valores:', this.form.value);
-  console.log('errores email:', this.form.get('email')?.errors);
-  console.log('errores password:', this.form.get('password')?.errors);
+  
+   
   if (this.form.invalid) return;
 
   this.auth.login(this.form.value).subscribe({
     next: (res: any) => {
-      localStorage.setItem('token', res.access_token); // ✅ FastAPI retorna "access_token"
+      localStorage.setItem('token', res.access_token); //  FastAPI retorna "access_token"
       console.log('Login exitoso');
+      this.router.navigate(['/dashboard']);
     },
     error: (err: any) => {
       console.error('Status:', err.status);
